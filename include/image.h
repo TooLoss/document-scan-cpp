1572ec0e5e9901da6942e551d067cae981abcb45
B_Loss <bilele.elhaddadi@gmail.com>
2025-11-20 18:46:56 +0100

    feat: add specification of necessary functions

--- include/image.h
+++ include/image.h
@@ -4,4 +4,15 @@
 
 class Image : Matrix2D<uint8_t> {
 
+public:
+  /**
+   * Connected Component Labeling
+   * Detect islands in the image. Islands are separated with background
+   * (ignored_value).
+   *
+   * @param ignored_value
+   * @return matrix with labels
+   */
+    Matrix2D connected_component_labeling(uint8_t ignored_value);
+
 };
