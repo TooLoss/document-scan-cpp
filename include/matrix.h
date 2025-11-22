8490be77bc4b63c45b18175129ba28db698b8f1d
Bilele <bilele.elhaddadi@gmail.com>
2025-11-22 18:00:46 +0100

    feat: operator between Coordinate and Integer

--- include/matrix.h
+++ include/matrix.h
@@ -1,4 +1,5 @@
 #include <vector>
+#include <stdint.h>
 #include <span>
 
 struct Coord {
@@ -6,6 +7,18 @@ struct Coord {
     uint32_t y;
 
     Coord(uint32_t ix, uint32_t iy) : x(ix), y(iy) {};
+
+    bool operator>=(int i) const {
+        return (this->x >= i) && (this->y >= i);
+    }
+
+    bool operator>(int i) const {
+        return (this->x > i) && (this->y > i);
+    }
+
+    bool operator==(int i) const {
+        return (this->x == i) && (this->y == i);
+    }
 }; 
 
 template <typename T>
@@ -15,6 +28,10 @@ public:
     Matrix2D(std::vector<T>&& val) : buffer(std::move(val)), values(val) {};
 
     std::span<T> values;
+
+    uint32_t to_vector_index(Coord coord);
+
+    bool is_in_range(Coord coord);
 
     /**
     * Get value of the matrixa type at x y coordinates.
@@ -33,7 +50,7 @@ public:
     void set_pixel_value(Coord coord, T value);
 
     template <typename U>
-    void convolve(Matrix2D<U> c_matrix);
+        void convolve(Matrix2D<U>& c_matrix);
 
     uint32_t get_rows() { return rows; }
     uint32_t get_columns() { return columns; }
@@ -45,5 +62,6 @@ protected:
     uint32_t rows;
     uint32_t columns;
 
-    void convolve_at(Coord coord, Matrix2D c_matrix);
+    template <typename U>
+        T convolve_at(Coord coord, Matrix2D<U>& c_matrix);
 };
